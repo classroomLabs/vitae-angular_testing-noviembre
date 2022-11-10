5c386ea75fa851e3e143a9ee0b4914fa6da1671f
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-10 17:28:26 +0100

    test: stores

---
+++ src/app/services/api.store.spec.ts
@@ -0,0 +1,57 @@
+import { ApiStore } from './api.store';
+describe('The ApiStore class ', () => {
+  it('should set initial state', () => {
+    const sut = new ApiStore();
+    sut.selectState$().subscribe((actual) => {
+      const expected = { isWorking: false, error: '', data: [] };
+      expect(actual).toEqual(expected);
+    });
+  });
+  it('should change the state', () => {
+    const sut = new ApiStore();
+    const input = [
+      { destination: 'The Moon', startDate: new Date('2022-11-10') },
+    ];
+    sut.setData(input);
+    sut.selectState$().subscribe((actual) => {
+      const expected = {
+        isWorking: false,
+        error: '',
+        data: [{ destination: 'The Moon', startDate: new Date('2022-11-10') }],
+      };
+      expect(actual).toEqual(expected);
+    });
+  });
+  it('should set is working', () => {
+    const store = new ApiStore();
+    store.setIsWorking();
+    store.selectState$().subscribe((actual) => {
+      const expected = true;
+      expect(actual.isWorking).toEqual(expected);
+    });
+  });
+  it('should set data', () => {
+    const store = new ApiStore();
+    const data = [{ id: 1 }, { id: 2 }];
+    store.setData(data);
+    store.selectState$().subscribe((actual) => {
+      const expected = data;
+      expect(actual.data).toEqual(expected);
+    });
+  });
+  it('should add item', () => {
+    const store = new ApiStore();
+    const data = [{ destination: 'The Moon' }, { destination: 'Mars' }];
+    store.setData(data);
+    const newItem = { destination: 'Earth orbit' };
+    store.addItem(newItem);
+    store.selectState$().subscribe((actual) => {
+      const expected = [
+        { destination: 'The Moon' },
+        { destination: 'Mars' },
+        { destination: 'Earth orbit' },
+      ];
+      expect(actual.data).toEqual(expected);
+    });
+  });
+});
--- src/app/services/base.store.spec.ts
+++ src/app/services/base.store.spec.ts
@@ -1,5 +1,6 @@
 import { BaseStore } from './base.store';
-describe('Store', () => {
+
+describe('The BaseStore class', () => {
   it('should create an instance', () => {
     // Arrange
     const initialState = {};
@@ -11,12 +12,46 @@ describe('Store', () => {
 
   it('should set initial state', () => {
     // Arrange
-    const input = {};
+    type State = { destination: string; startDate: Date };
+    const input = {
+      destination: 'The Moon',
+      startDate: new Date('2022-11-10'),
+    };
     // Act
-    const sut = new BaseStore(input);
+    const sut = new BaseStore<State>(input);
     const actual = sut.getState();
     // Assert
-    const expected = {};
+    const expected = {
+      destination: 'The Moon',
+      startDate: new Date('2022-11-10'),
+    };
     expect(actual).toEqual(expected);
+    expect(actual).not.toBe(input);
+  });
+
+  it('should set notify changes', () => {
+    // Arrange
+    type State = { destination: string; startDate: Date };
+    const initialState = {
+      destination: 'The Moon',
+      startDate: new Date('2022-11-10'),
+    };
+    const sut = new BaseStore<State>(initialState);
+    // Act
+    const input = {
+      destination: 'Mars',
+    };
+    sut.setState(input);
+    sut
+      .select$((state) => state)
+      .subscribe((actual) => {
+        // Assert
+        const expected = {
+          destination: 'Mars',
+          startDate: new Date('2022-11-10'),
+        };
+        expect(actual).toEqual(expected);
+        expect(actual).not.toBe(initialState);
+      });
   });
 });
