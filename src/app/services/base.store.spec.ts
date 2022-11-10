ed5b1521d9d3c1c2a7c4a5127a9d0b2483e8e2eb
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-10 17:40:05 +0100

    feat: base test

--- src/app/services/base.store.spec.ts
+++ src/app/services/base.store.spec.ts
@@ -2,23 +2,17 @@ import { BaseStore } from './base.store';
 
 describe('The BaseStore class', () => {
   it('should create an instance', () => {
-    // Arrange
-    const initialState = {};
-    // Act
-    const actual = new BaseStore(initialState);
-    // Assert
-    expect(actual).toBeDefined();
+    expect(new BaseStore(null)).toBeTruthy();
   });
-
-  it('should set initial state', () => {
+  it('should return initial state after instantiation', () => {
     // Arrange
     type State = { destination: string; startDate: Date };
-    const input = {
+    const input: State = {
       destination: 'The Moon',
       startDate: new Date('2022-11-10'),
     };
+    const sut = new BaseStore(input);
     // Act
-    const sut = new BaseStore<State>(input);
     const actual = sut.getState();
     // Assert
     const expected = {
@@ -28,30 +22,101 @@ describe('The BaseStore class', () => {
     expect(actual).toEqual(expected);
     expect(actual).not.toBe(input);
   });
-
-  it('should set notify changes', () => {
+  it('should return the last state set', () => {
     // Arrange
-    type State = { destination: string; startDate: Date };
-    const initialState = {
+    type State = { destination: string };
+    const initialState: State = {
       destination: 'The Moon',
-      startDate: new Date('2022-11-10'),
     };
-    const sut = new BaseStore<State>(initialState);
-    // Act
-    const input = {
+    const sut = new BaseStore(initialState);
+    const input: State = {
       destination: 'Mars',
     };
+    // Act
     sut.setState(input);
+    const actual = sut.getState();
+    // Assert
+    const expected = {
+      destination: 'Mars',
+    };
+    expect(actual).toEqual(expected);
+  });
+  it('should change state with partial mutations', () => {
+    // Arrange
+    type State = { destination: string; price: number };
+    const initialState: State = {
+      destination: 'The Moon',
+      price: 100,
+    };
+    const sut = new BaseStore(initialState);
+    const input: Partial<State> = {
+      price: 200,
+    };
+    // Act
+    sut.setState(input);
+    const actual = sut.getState();
+    // Assert
+    const expected = {
+      destination: 'The Moon',
+      price: 200,
+    };
+    expect(actual).toEqual(expected);
+  });
+  it('should select full state', () => {
+    // Arrange
+    type State = { destination: string };
+    const input: State = {
+      destination: 'The Moon',
+    };
+    const sut = new BaseStore(input);
+    // Act
     sut
       .select$((state) => state)
       .subscribe((actual) => {
         // Assert
         const expected = {
-          destination: 'Mars',
-          startDate: new Date('2022-11-10'),
+          destination: 'The Moon',
         };
         expect(actual).toEqual(expected);
-        expect(actual).not.toBe(initialState);
+      });
+  });
+  it('should select partial state', () => {
+    // Arrange
+    type State = { destination: string };
+    const input: State = {
+      destination: 'The Moon',
+    };
+    const sut = new BaseStore(input);
+    // Act
+    sut
+      .select$((state) => state.destination)
+      .subscribe((actual) => {
+        // Assert
+        const expected = 'The Moon';
+        expect(actual).toEqual(expected);
+      });
+  });
+  it('should select partial state with multiple subscriptions', () => {
+    // Arrange
+    type State = { destination: string };
+    const input: State = {
+      destination: 'The Moon',
+    };
+    const sut = new BaseStore(input);
+    // Act
+    sut
+      .select$((state) => state.destination)
+      .subscribe((actual) => {
+        // Assert
+        const expected = 'The Moon';
+        expect(actual).toEqual(expected);
+      });
+    sut
+      .select$((state) => state.destination)
+      .subscribe((actual) => {
+        // Assert
+        const expected = 'The Moon';
+        expect(actual).toEqual(expected);
       });
   });
 });
