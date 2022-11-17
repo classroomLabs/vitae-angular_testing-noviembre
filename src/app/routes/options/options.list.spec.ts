bdce80b33bf61df5ce7ea5877cb2b52a4341fec0
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-17 19:06:10 +0100

    feat: options list

--- src/app/routes/options/options.list.spec.ts
+++ src/app/routes/options/options.list.spec.ts
@@ -1,4 +1,7 @@
+import { ChangeDetectionStrategy } from '@angular/compiler';
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { OptionsList } from './options.list';
 
@@ -8,9 +11,12 @@ describe('OptionsList', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OptionsList ]
+      declarations: [OptionsList],
     })
-    .compileComponents();
+      .overrideComponent(OptionsList, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(OptionsList);
     component = fixture.componentInstance;
@@ -19,5 +25,52 @@ describe('OptionsList', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should present a list of options', () => {
+    component.options = [{ label: 'test', value: '0' }];
+    fixture.detectChanges();
+    const actual = fixture.nativeElement.querySelectorAll('li');
+    expect(actual.length).toBe(1);
+  });
+});
+
+@Component({
+  template: `
+    <app-options-list [options]="input" (delete)="onDeleteClick($event)">
+    </app-options-list>
+  `,
+})
+class HostComponent {
+  input = [{ label: 'test', value: '0' }];
+  token = null;
+  onDeleteClick(event: any) {
+    this.token = event;
+  }
+}
+
+fdescribe('The Options List hosted', () => {
+  let component: HostComponent;
+  let fixture: ComponentFixture<HostComponent>;
+  let native: any;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OptionsList, HostComponent],
+    }).compileComponents();
+    fixture = TestBed.createComponent(HostComponent);
+    component = fixture.componentInstance;
+    native = fixture.nativeElement;
+    fixture.detectChanges();
+  });
+
+  it('should sent an array to the list component', () => {
+    const actualItems = native.querySelectorAll('li');
+    expect(actualItems.length).toBe(1);
+  });
+  it('should emit delete event when click', () => {
+    const actualButton = fixture.debugElement.query(By.css('li>span'));
+    actualButton.triggerEventHandler('click', null);
+    expect(component.token).toBeTruthy();
   });
 });
--- src/test.ts
+++ src/test.ts
@@ -34,8 +34,9 @@ const storeContext = require.context('./', true, /store.spec\.ts$/);
 const servicesContext = require.context('./', true, /service.spec\.ts$/);
 const pipesContext = require.context('./', true, /pipe.spec\.ts$/);
 const componentsContext = require.context('./', true, /component.spec\.ts$/);
+const listContext = require.context('./', true, /list.spec\.ts$/);
 const formsContext = require.context('./', true, /form.spec\.ts$/);
 const fullContext = require.context('./', true, /\.spec\.ts$/);
-const context = componentsContext;
+const context = listContext;
 // And load the modules.
 context.keys().forEach(context);
