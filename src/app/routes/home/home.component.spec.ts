2e65d3a7b9cda509f9fd08fa222b932448702383
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-22 17:13:33 +0100

    test: home compoent controller

--- src/app/routes/home/home.component.spec.ts
+++ src/app/routes/home/home.component.spec.ts
@@ -1,16 +1,89 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ApiService } from '@services/api.service';
+import { of } from 'rxjs';
 
 import { HomeComponent } from './home.component';
+import { HomeService } from './home.service';
 
-describe('HomeComponent', () => {
+describe('The Home Component with fake imports ', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [HomeComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+fdescribe('The Home Component with nested dependencies ', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+  let homeServiceStub: jasmine.SpyObj<HomeService>;
+  let apiServiceStub: jasmine.SpyObj<ApiService>;
+  beforeEach(async () => {
+    homeServiceStub = jasmine.createSpyObj('HomeService', [
+      'loadTrips',
+      'selectTrips$',
+    ]);
+    homeServiceStub.loadTrips.and.returnValue();
+    apiServiceStub = jasmine.createSpyObj('ApiService', ['getTrips$']);
+    apiServiceStub.getTrips$.and.returnValue(of([]));
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+      providers: [
+        { provide: HomeService, useValue: homeServiceStub },
+        { provide: ApiService, useValue: apiServiceStub },
+      ],
     })
-    .compileComponents();
+      .overrideComponent(HomeComponent, {
+        set: {
+          providers: [{ provide: HomeService, useValue: homeServiceStub }],
+        },
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+class HomeServiceStub {
+  loadTrips() {}
+  selectTrips$() {
+    return of([{ isWorking: false, error: '', data: [] }]);
+  }
+}
+
+fdescribe('The Home Component with stubbed class ', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+    })
+      .overrideComponent(HomeComponent, {
+        set: {
+          providers: [{ provide: HomeService, useClass: HomeServiceStub }],
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
